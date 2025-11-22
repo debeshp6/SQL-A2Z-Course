73cd1121bd44fe607d4bb94ae49d23d82a444f01
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-22 23:08:48 +0530

    Show first name and last name of patients who does not have allergies. (null)

---
+++ 002.sql
@@ -0,0 +1,23 @@
+/* Q: Show first name and last name of patients who does not have allergies. (null)
+
+Table Info: patient
+
+| Column Name   | Type     | Details        |
+|---------------|----------|----------------|
+| patient_id    | INT      | primary key    |
+| first_name    | TEXT     |                |
+| last_name     | TEXT     |                |
+| gender        | CHAR(1)  |                |
+| birth_date    | DATE     |                |
+| city          | TEXT     |                |
+| province_id   | CHAR(2)  | primary key    |
+| allergies     | TEXT     |                |
+| height        | INT      |                |
+| weight        | INT      |                |
+
+*/
+
+-- SOLUTION:
+
+select first_name, last_name from patients
+where allergies is NULL;
