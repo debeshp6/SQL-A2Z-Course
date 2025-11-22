44d4db8f26e229a7d58de39d2f7cbedb1fd26916
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-22 23:13:10 +0530

    Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)

---
+++ 004.sql
@@ -0,0 +1,24 @@
+/* Q: Show first name and last name of patients that weight within the range of 100 to 120 (inclusive).
+
+Table Info: patient
+
+| Column Name   | Type     | Details        |
+|---------------|----------|----------------|
+| patient_id    | INT      | primary key    |
+| first_name    | TEXT     |                |
+| last_name     | TEXT     |                |
+| gender        | CHAR(1)  |                |
+| birth_date    | DATE     |                |
+| city          | TEXT     |                |
+| province_id   | CHAR(2)  | primary key    |
+| allergies     | TEXT     |                |
+| height        | INT      |                |
+| weight        | INT      |                |
+
+*/
+
+-- SOLUTION:
+
+select first_name, last_name 
+from patients
+where weight between 100 and 120;
