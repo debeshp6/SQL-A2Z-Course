9cb1d90d021e1ffed0762e5c1d56fb070324654d
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-23 22:14:50 +0530

    Based on the cities that our patients live in, show unique cities that are in province_id 'NS'.

---
+++ 014.sql
@@ -0,0 +1,26 @@
+/* Q: Based on the cities that our patients live in, show unique cities that are in province_id 'NS'.
+
+Table Info: patients
+
++--------------+----------+
+| Column Name  | Type     |
++--------------+----------+
+| patient_id   | INT      |
+| first_name   | TEXT     |
+| last_name    | TEXT     |
+| gender       | CHAR(1)  |
+| birth_date   | DATE     |
+| city         | TEXT     |
+| province_id  | CHAR(2)  |
+| allergies    | TEXT     |
+| height       | INT      |
+| weight       | INT      |
++--------------+----------+
+
+*/
+
+-- SOLUTION:
+
+select distinct city as unique_cities 
+from patients
+where province_id = 'NS';
