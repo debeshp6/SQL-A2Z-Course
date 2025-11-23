08f24f4067480ac4a54a4c76dd20c21848b6b293
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-23 22:20:26 +0530

    Write a query to find the first_name, last name and birth date of patients who has height greater than 160 and weight greater than 70.

---
+++ 015.sql
@@ -0,0 +1,26 @@
+/* Q: Write a query to find the first_name, last name and birth date of patients who has height greater than 160 and weight greater than 70.
+
+Table Info: admissions
+
++--------------+----------+
+| Column Name  | Type     |
++--------------+----------+
+| patient_id   | INT      |
+| first_name   | TEXT     |
+| last_name    | TEXT     |
+| gender       | CHAR(1)  |
+| birth_date   | DATE     |
+| city         | TEXT     |
+| province_id  | CHAR(2)  |
+| allergies    | TEXT     |
+| height       | INT      |
+| weight       | INT      |
++--------------+----------+
+
+*/
+
+-- SOLUTION:
+
+select first_name, last_name, birth_date
+from patients
+where height > 160 and weight > 70;
