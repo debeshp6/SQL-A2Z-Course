2b2d19464b9b207311dd7d862e11224eb05df237
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-23 22:30:56 +0530

    Show unique birth years from patients and order them by ascending.

---
+++ 017.sql
@@ -0,0 +1,26 @@
+/* Q: Show unique birth years from patients and order them by ascending.
+
+Table Info: patients
+
++--------------+----------+
+| Column Name  | Type     |
++--------------+----------+
+| patient_id   | INT      |
+| first_name   | TEXT     |
+| last_name    | TEXT     |
+| gender       | CHAR(1)  |
+| birth_date   | DATE     |
+| city         | TEXT     |
+| province_id  | CHAR(2)  |
+| allergies    | TEXT     |
+| height       | INT      |
+| weight       | INT      |
++--------------+----------+
+
+*/
+
+-- SOLUTION:
+
+select distinct year(birth_date) as birth_year
+from patients
+order by birth_year asc;
