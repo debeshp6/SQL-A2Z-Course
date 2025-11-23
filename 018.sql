a900362e3a256c782601f1ee13211da23a1568be
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-24 00:57:30 +0530

    Show unique first names from the patients table which only occurs once in the list.

---
+++ 018.sql
@@ -0,0 +1,29 @@
+/* Q: Show unique first names from the patients table which only occurs once in the list.
+
+For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
+
+Table Info: patients
+
++--------------+----------+
+| Column Name  | Type     |
++--------------+----------+
+| patient_id   | INT      |
+| first_name   | TEXT     |
+| last_name    | TEXT     |
+| gender       | CHAR(1)  |
+| birth_date   | DATE     |
+| city         | TEXT     |
+| province_id  | CHAR(2)  |
+| allergies    | TEXT     |
+| height       | INT      |
+| weight       | INT      |
++--------------+----------+
+
+*/
+
+-- SOLUTION:
+
+select first_name
+from patients
+group by first_name
+having count(*) = 1;
