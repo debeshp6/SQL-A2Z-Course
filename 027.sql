862b1c8e5215ece4182f67f8ef0f17d50ca668db
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-24 21:35:03 +0530

    Show all allergies ordered by popularity. Remove NULL values from query.

---
+++ 027.sql
@@ -0,0 +1,28 @@
+/* Q: Show all allergies ordered by popularity. Remove NULL values from query.
+
+Table Info: patients
+
++--------------+----------+
+| Column Name  | Type     |
++--------------+----------+
+| patient_id   | INT      |
+| first_name   | TEXT     |
+| last_name    | TEXT     |
+| gender       | CHAR(1)  |
+| birth_date   | DATE     |
+| city         | TEXT     |
+| province_id  | CHAR(2)  |
+| allergies    | TEXT     |
+| height       | INT      |
+| weight       | INT      |
++--------------+----------+
+
+*/
+
+-- SOLUTION:
+
+select allergies, count(*) as total_diagnosis
+from patients
+where allergies is not null
+group by allergies
+order by total_diagnosis desc;
