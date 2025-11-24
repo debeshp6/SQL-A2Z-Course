fa1a9c59df6dfa636fc3e5a4bb902e6ddf6a7014
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-25 00:54:28 +0530

    Show the difference between the largest weight and smallest weight for patients with the last name 'Maroni'

---
+++ 031.sql
@@ -0,0 +1,26 @@
+/* Q: Show the difference between the largest weight and smallest weight for patients with the last name 'Maroni'
+
+Table Info: patients
+
++--------------+----------+
+| Column Name  | Type     |
++--------------+----------+
+| patient_id   | INT      |
+| first_name   | TEXT     |
+| last_name    | TEXT     |
+| gender       | CHAR(1)  |
+| birth_date   | DATE     |
+| city         | TEXT     |
+| province_id  | CHAR(2)  |
+| allergies    | TEXT     |
+| height       | INT      |
+| weight       | INT      |
++--------------+----------+
+
+*/
+
+-- SOLUTION:
+
+select (max(weight) - min(weight)) as weight_delta
+from patients
+where last_name = 'Maroni';
