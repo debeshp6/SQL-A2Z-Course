a0806fc15de936835e8e3c12372f40db97b70569
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-27 22:29:35 +0530

    Display a single row with max_visits, min_visits, average_visits where the maximum, minimum and average number of admissions per day is calculated. Average is rounded to 2 decimal places.

---
+++ 042.sql
@@ -0,0 +1,25 @@
+/* Q: Display a single row with max_visits, min_visits, average_visits where the maximum, minimum and average number of admissions per day is calculated. Average is rounded to 2 decimal places.
+
++----------------------+----------+
+| Column Name          | Type     |
++----------------------+----------+
+| patient_id           | INT      |
+| admission_date       | DATE     |
+| discharge_date       | DATE     |
+| diagnosis            | TEXT     |
+| attending_doctor_id  | INT      |
++----------------------+----------+
+
+*/
+
+-- SOLUTION:
+
+select
+	max(visits) as max_visits,
+    min(visits) as min_visits,
+    round(avg(visits), 2) as average_visits
+from (
+  	select admission_date, count(*) as visits
+  	from admissions
+  	group by admission_date
+);
