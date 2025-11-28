5488d179431131dbf1b7250def42da733dfa0d61
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-11-28 23:14:42 +0530

    Show patient_id, weight, height, isObese from the patients table.

---
+++ 045.sql
@@ -0,0 +1,39 @@
+/* Q: Show patient_id, weight, height, isObese from the patients table.
+
+Display isObese as a boolean 0 or 1.
+
+Obese is defined as weight(kg)/(height(m)2) >= 30.
+
+weight is in units kg.
+height is in units cm.
+
+Table Info: patients
+
++--------------+----------+
+| Column Name  | Type     |
++--------------+----------+
+| patient_id   | INT      |
+| first_name   | TEXT     |
+| last_name    | TEXT     |
+| gender       | CHAR(1)  |
+| birth_date   | DATE     |
+| city         | TEXT     |
+| province_id  | CHAR(2)  |
+| allergies    | TEXT     |
+| height       | INT      |
+| weight       | INT      |
++--------------+----------+
+
+*/
+
+-- SOLUTION:
+
+select
+	patient_id,
+    weight,
+    height,
+case
+	when weight / power(height/100.0, 2) >= 30 then 1
+    else 0
+end as isObese
+from patients;
